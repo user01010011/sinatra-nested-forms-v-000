fb3337f57b705fd9c21665bf5e74acc4f94546d4
Shasha Yan <shashayan@gmail.com>
2021-06-02 19:16:39 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    attr_reader :name, :weight, :height
+
+    @@pirates = [ ]
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
 end
